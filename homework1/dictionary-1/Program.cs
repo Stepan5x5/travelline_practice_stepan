e43fe7fa1ddd7aacca4342b1b9cf947dc64ca8a0
Макаров Степан <162582526+Stepan5x5@users.noreply.github.com>
2024-04-01 17:45:48 +0300

    Добавление домашнего задания попытка 9

---
+++ homework1/dictionary-1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, string> interpreter = new Dictionary<string, string>();
+            Dictionary<string, string> reverseDictionary = new Dictionary<string, string>();
+            while ( true )
+            {
+
+                Console.WriteLine( "Меню:" );
+                Console.WriteLine( "1. Добавить перевод " );
+                Console.WriteLine( "2. Удалить перевод " );
+                Console.WriteLine( "3. Изменить перевод " );
+                Console.WriteLine( "4. Перевести слово " );
+                Console.WriteLine( "5. Выйти из программы" );
+
+                Console.Write( "Выберите команду: " );
+
+                string choice = Console.ReadLine();
+                switch ( choice )
+                {
+                    case "1":
+                        Translation.Add( interpreter, reverseDictionary );
+                        break;
+                    case "2":
+                        Translation.Remove( interpreter );
+                        break;
+                    case "3":
+                        Translation.Change( interpreter );
+                        break;
+                    case "4":
+                        Translation.Prepared( interpreter, reverseDictionary );
+                        break;
+                    case "5":
+                        Console.WriteLine( "Программа завершена" );
+                        return;
+                    default:
+                        Console.WriteLine( "Неизвестная команда." );
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ homework1/dictionary-1/Translation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    public class Translation
+    {
+        public static void Add( Dictionary<string, string> interpreter, Dictionary<string, string> reverseDictionary )
+        {
+            Console.WriteLine( "Напишите слово для которого хотите добавить перевод:" );
+            string word = Console.ReadLine();
+            Console.WriteLine( $"Введите перевод для слова: {word} Ваш перевод:" );
+            string converted = Console.ReadLine();
+            interpreter[ word ] = converted;
+            reverseDictionary[ converted ] = word;
+            Console.WriteLine( $"Ваш перевод добавлен. Результат: {word} - {converted}" );
+        }
+        public static void Remove( Dictionary<string, string> interpreter )
+        {
+            Console.WriteLine( "Введите слово которое хотите удалить:" );
+            string word = Console.ReadLine();
+            if ( interpreter.ContainsKey( word ) )
+            {
+                interpreter.Remove( word );
+                Console.WriteLine( "Слово удалено" );
+            }
+            else
+            {
+                Console.WriteLine( $"Слово {word} не найдено." );
+            }
+        }
+        public static void Change( Dictionary<string, string> interpreter )
+        {
+            Console.WriteLine( "Напишите слово для которого хотите изминить перевод:" );
+            string word = Console.ReadLine();
+            Console.WriteLine( $"Напишите новый перевод для слова {word}:" );
+            string converted = Console.ReadLine();
+            if ( interpreter.ContainsKey( word ) )
+            {
+                interpreter[ word ] = converted;
+                Console.WriteLine( $"Ваш перевод изменен. Результат: {word} - {converted}" );
+            }
+            else
+            {
+                Console.WriteLine( $"Слово {word} не найдено." );
+            }
+        }
+        public static void Prepared( Dictionary<string, string> interpreter, Dictionary<string, string> reverseDictionary )
+        {
+            Console.WriteLine( "Напишите слово которое хотите перевести" );
+            string word = Console.ReadLine();
+            if ( interpreter.ContainsKey( word ) )
+            {
+                Console.WriteLine( $"Ваш перевод готов. Результат: {word} - {interpreter[ word ]}" );
+            }
+            else if ( reverseDictionary.ContainsKey( word ) )
+            {
+                Console.WriteLine( $"Обратный перевод слова готов. Результат: {word} - {reverseDictionary[ word ]}" );
+            }
+            else
+            {
+                Console.WriteLine( $"Слово {word} не найдено." );
+            }
+        }
+    }
+}
